1a247f6811b47fa91c01b8c38c59f01e4096d6d8
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-03 08:03:58 +0000

    [start]first commit

---
+++ bookers2-debug4/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :follower, :followed
+    end
+  end
+  resources :relationships, only:[:create, :destroy]
+  resources :books do
+    resource :favorites, only:[:create, :destroy]
+    resource :book_comments, only:[:create, :destroy]
+  end
+  root 'home#top'
+  get 'home/about' => 'home#about'
+  get 'home/top'
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
+
+end
