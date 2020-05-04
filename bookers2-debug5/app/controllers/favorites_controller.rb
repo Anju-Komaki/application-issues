a3aae40697cb22320be76215971701e88d4ed5de
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-04 06:35:26 +0000

    [modified]favorite ajax

--- bookers2-debug5/app/controllers/favorites_controller.rb
+++ bookers2-debug5/app/controllers/favorites_controller.rb
@@ -1,14 +1,23 @@
 class FavoritesController < ApplicationController
 	def create
-		#@book = Book.find(params[:book_id])
-		favorite = current_user.favorites.build(book_id: params[:book_id])
+		@book = Book.find(params[:book_id])
+		favorite = @book.favorites.new(user_id: current_user.id)
 		favorite.save
-		redirect_back(fallback_location: books_path)
 	end
+
 	def destroy
-		#@book = Book.find(params[:book_id])
-		favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
+		@book = Book.find(params[:book_id])
+		favorite = current_user.favorites.find_by(book_id: @book.id)
 		favorite.destroy
-		redirect_back(fallback_location: books_path)
 	end
+
+	private
+    def redirect
+    	case params[:redirect_id].to_i
+    when 0
+      	redirect_to books_path
+    when 1
+      	redirect_to book_path(@book)
+    end
+  end
 end
